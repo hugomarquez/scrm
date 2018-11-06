c72a5cba4fe739c44f49f901ab6e461b00795c86
hugomarquez <hugomarquez.dev@gmail.com>
2018-11-05 23:31:04 -0600

    adds activity to dashboard#index

--- app/controllers/crm/dashboard_controller.rb
+++ app/controllers/crm/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class Crm::DashboardController < ApplicationController
   def index
+    @activities = PublicActivity::Activity.includes(:owner, :trackable).order(created_at: :desc).page(params[:activity_page]).per(5)
   end
 end
