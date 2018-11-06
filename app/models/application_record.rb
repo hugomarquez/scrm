4f6302f02cb768c8164d61f1077cb4796407a427
hugomarquez <hugomarquez.dev@gmail.com>
2018-11-06 01:14:30 -0600

    adds simple user admin seed

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,5 +2,9 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   scope :recent, -> { where(created_at: 5.days.ago.. Time.current).order(created_at: :desc) }
+
+  def uid
+    slug || SecureRandom.urlsafe_base64(5, false)
+  end
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Core::User.create(email: 'admin@example.com', password: 'password', password_confirmation: 'password', person_attributes: {first_name: 'Default', last_name: 'Admin'})
