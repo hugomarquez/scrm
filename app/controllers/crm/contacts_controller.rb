846dd5d8073a69ddbad7f3968db0a734f6e92203
hugomarquez <hugomarquez.dev@gmail.com>
2018-11-14 02:00:49 -0600

    adds lookup from contacts to accounts

--- app/controllers/crm/contacts_controller.rb
+++ app/controllers/crm/contacts_controller.rb
@@ -50,15 +50,11 @@ class Crm::ContactsController < ApplicationController
   def update
     authorize @contact
     @contact.attributes = contact_params
-    if !contact_params[:account_label].blank? && contact_params[:account_id].blank?
-      @account = Crm::Account.where('name Like :name OR number LIKE :name', name: contact_params[:account_label]).first
-      @contact.account = @account if @account
-    elsif contact_params[:account_label].blank?
-      @contact.account_id = nil
-    else
-      @contact.account_id = contact_params[:account_id]
+    # If no contact is selected
+    if contact_params[:account_label].blank?
+      @contact.account = nil
     end
-
+    
     if @contact.valid?
       @contact.save
       flash[:success] = t('controllers.crm/contacts.update.success')
@@ -83,8 +79,8 @@ class Crm::ContactsController < ApplicationController
 
   def contact_params
     params.require(:crm_contact).permit(
-      :number, :language, :description, :created_by_id, :account_label, :account_id,
-      :lead_source, :level,
+      :account_label, :account_id, :description, :created_by_id, :lead_source,
+      :level, :language, :number,
       person_attributes:
         [:_destroy, :id, :title, :first_name, :last_name, :phone, :home_phone,
           :other_phone, :email, :assistant, :asst_phone, :extension, :mobile,
