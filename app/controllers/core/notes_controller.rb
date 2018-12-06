26af11e02ac654e418a1c7feb03b26a47e4c927d
Alan Monreal <44030495+AlanMonreal@users.noreply.github.com>
2018-12-06 01:06:42 -0600

    Update notes_controller.rb

--- app/controllers/core/notes_controller.rb
+++ app/controllers/core/notes_controller.rb
@@ -1,11 +1,15 @@
+#Declaracion de la clase NotesController
 class Core::NotesController < ApplicationController
   before_action :set_note, only:[:edit, :show, :update, :destroy]
 
+  #Metodo new. Le asigna un valor a @note y se llama la funcion authorize para comprobar que el usuario tenga 
+  #los permisos necesarios. Devuelve true si cuenta con la autorizacion, de lo contrario lanza una excepcion
   def new
     @note = Core::Note.new(created_by: current_core_user)
     authorize @note
   end
 
+  #Metodo edit. Comprueba el nivel de privilegios y despues se le agrega el valor related_to dependiendo de los atributos
   def edit
     authorize @note
     if @note.noteable
@@ -17,10 +21,13 @@ class Core::NotesController < ApplicationController
     end
   end
 
+  #Metodo show. Solamente comprueba los privilegios del usuario
   def show
     authorize @note
   end
 
+  #Metodo create. Crea un nuevo note, le agrega el nombre del usuario creador, comprueba sus privilegios, se le asigna un id.
+  #Si es valido lo guarda y te redirije a core_root_path, si no crea una respuesta nueva.
   def create
     @note = Core::Note.new(params_without_virtual_attributes)
     @note.created_by = current_core_user
@@ -35,6 +42,8 @@ class Core::NotesController < ApplicationController
 
   end
 
+  #Metodo update. Checa los privilegios, le asigna unos parametros vacios y genera un nuevo id.
+  #Comprueba que sea valido y lo guarda
   def update
     authorize @note
     @note.attributes = params_without_virtual_attributes
@@ -47,6 +56,7 @@ class Core::NotesController < ApplicationController
     end
   end
 
+  #Metodo destroy. Comprueba privilegios del usuario y, si se destruye el objeto te regresa a core_root_path
   def destroy
     authorize @note
     if @note.destroy
@@ -56,16 +66,19 @@ class Core::NotesController < ApplicationController
 
   private
 
+  #Metodo set_note. Se le asigna un valor a @note
   def set_note
     @note = Core::Note.includes(:noteable).friendly.find(params[:id])
   end
 
+  #Metodo note_params. Asigna los valores requeridos y permitidos en note
   def note_params
     params.require(:core_note).permit(
       :private, :title, :body, :noteable_type, :noteable_id, :related_to
     )
   end
 
+  #Metodo params_without_virtual_attributes. Le desasigna el id y la relacion al objeto recibido.
   def params_without_virtual_attributes
     note_params.select do |p|
       p.exclude?('noteable_id') and p.exclude?('related_to')
